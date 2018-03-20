cd5c3f3aa5d403fd70981735b2b2f6c1e798ae8a
FearlessPoro <wojtek.franczuk@gmail.com>
2018-03-20 22:52:33 +0100

    Fixed some grammar mistakes and added an easier to understand tutorial

--- game.py
+++ game.py
@@ -7,9 +7,8 @@ class Game(object):
         for field in range(9):
             self.board.append(' ')
 
-
     def check_if_given_field_correct(self, field):
-        if field >= 0 and field <= 8:
+        if 0 <= field <= 8:
             if self.board[field] == ' ':
                 print("Your choice was saved.")
                 return True
@@ -17,20 +16,19 @@ class Game(object):
                 print("This field is already occupied. Please choose another free value.")
                 return False
         else:
-            print("Bad number, use one with 0-8 to make movement.")
+            print("Bad number, use a number from range 0-8 to make a move.")
             return False
 
-
-    def first_user_motion(self):
-        field = int(input("Now first player play. Give one position using numbers 0-8:   "))
+    def first_user_move(self):
+        field = int(input("First player's move. Choose a spot using numbers 0-8:   "))
         if self.check_if_given_field_correct(field):
             self.board[field] = "O"
             return True
         else:
             return False
 
-    def second_user_motion(self):
-        field = int(input("Now second player play. Give one position using numbers 0-8:   "))
+    def second_user_move(self):
+        field = int(input("Second player's move. Choose a spot using numbers 0-8:   "))
         if self.check_if_given_field_correct(field):
             self.board[field] = "X"
             return True
@@ -46,23 +44,35 @@ class Game(object):
                 print("|", end = '')
         print("\n-----------")
 
+    @staticmethod
+    def mock_board():
+        print("How to play:\n", "Choose where do you want to play your dot or cross,\n",
+              "by typing a number corresponding to the field below")
+        for field in range(9):
+            if field%3 == 0:
+                print("\n-----------")
+            print(" " + str(field) + " ", end = '')
+            if (field+1)%3 != 0:
+                print("|", end = '')
+        print("\n-----------")
+
     def game(self):
         counter = 0
-        print("Now we are starting the game!")
+        Game.mock_board()
+        print("New game started!")
         self.print_board()
         first_player_play = True
         while counter <= 8:
             if first_player_play:
-                if self.first_user_motion():
+                if self.first_user_move():
                     first_player_play = False
                     self.print_board()
                     counter += 1
             else:
-                if self.second_user_motion():
+                if self.second_user_move():
                     first_player_play = True
                     self.print_board()
                     counter += 1
-
 
 
 new_game = Game()
